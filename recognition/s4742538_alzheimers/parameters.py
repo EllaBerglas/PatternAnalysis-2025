dee339d4f15c65c659ca456f00864a321258929b
EllaBerglas <ella@berglas.org>
2025-10-26 22:48:33 +1000

    Cleaned up parameters

--- recognition/s4742538_alzheimers/parameters.py
+++ recognition/s4742538_alzheimers/parameters.py
@@ -1,46 +1,59 @@
 """
 Holds general parameters for the project
 """
-
+# uncomment fo ruse on colab
 # from google.colab import drive
 # drive.mount('/content/gdrive')
 # DATA_ROOT = "./gdrive/My Drive/Colab Notebooks/data/AD_NC/"
 
 DATA_ROOT = "./data/AD_NC/"
+MODEL_SIZE = "SMALL" # "SMALL", "TINY", "BASE"
 
-CHANNELS = 1 # per image
-IMAGE_SIZE = 384
-NORMALISATION_M = 0.1114
-NORMALISATION_SD = 0.2184
 BATCH_SIZE = 16
-
 LEARNING_RATE = 1e-4
 WEIGHT_DECAY = 1e-3
 EPOCHS = 15
+COMPILE = True
 
-MODEL_FILENAME = f"20Chan_{EPOCHS}_tiny_transform.pth"
+MODEL_FILENAME = f"20Chan_{EPOCHS}_{MODEL_SIZE}_best.pth"
 
-MODEL_CONFIG = { # tiny
+MODEL_CONFIG_TINY = { # tiny
     "in_chans": 20,
     "num_classes": 2,
     "depths": [3, 3, 9, 3],
     "dims": [96, 192, 384, 768]
 }
 
-# MODEL_CONFIG = { # small
-#     "in_chans": 20,
-#     "num_classes": 2,
-#     "depths": [3, 3, 27, 3],
-#     "dims": [96, 192, 384, 768],
-#     "drop_path_rate": 0.2  # dropout for residual connections
-# }
+MODEL_CONFIG_SMALL= { # small
+    "in_chans": 20,
+    "num_classes": 2,
+    "depths": [3, 3, 27, 3],
+    "dims": [96, 192, 384, 768],
+    "drop_path_rate": 0.2  # dropout for residual connections
+}
 
-# MODEL_CONFIG = { # base
-#     "in_chans": 20,
-#     "num_classes": 2,
-#     "depths": [3, 3, 27, 3],
-#     "dims": [128, 256, 512, 1024]
-# }
+MODEL_CONFIG_BASE = { # base
+    "in_chans": 20,
+    "num_classes": 2,
+    "depths": [3, 3, 27, 3],
+    "dims": [128, 256, 512, 1024]
+}
+
+RANDOM_STATE = 42
+NORMALISATION_M = 0.1114
+NORMALISATION_SD = 0.2184
+CHANNELS = 1 # per image (greyscale)
+IMAGE_SIZE = 224
+MODEL_CONFIG = MODEL_CONFIG_TINY
+
+match MODEL_SIZE:
+    case "TINY":
+        MODEL_CONFIG = MODEL_CONFIG_TINY
+    case "SMALL":
+        MODEL_CONFIG = MODEL_CONFIG_SMALL
+    case "BASE":
+        MODEL_CONFIG = MODEL_CONFIG_BASE
+        IMAGE_SIZE = 384
 
 import os 
 
