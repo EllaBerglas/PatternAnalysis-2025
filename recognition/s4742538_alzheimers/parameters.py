0291c60b17f8ff2200d47a57b6be0bb75ed0941f
EllaBerglas <ella@berglas.org>
2025-10-31 03:03:11 +1000

    extracted more parameters to the parameters file. Cleaned up module file

--- recognition/s4742538_alzheimers/parameters.py
+++ recognition/s4742538_alzheimers/parameters.py
@@ -10,61 +10,41 @@ DATA_ROOT = "./data/AD_NC/"
 MODEL_SIZE = "SMALL" # "SMALL", "TINY", "BASE"
 
 BATCH_SIZE = 16
-LEARNING_RATE = 1e-4
+LEARNING_RATE = 3e-4
 WEIGHT_DECAY = 1e-3
-EPOCHS = 15
+EPOCHS = 120
 COMPILE = True
+AUGMENTED_DS = True
+# NUM_CLASSES = 2 # for cross entropy loss only
+NUM_CLASSES = 1
+AUG_PROB = 1
+CHECKS = True
 
-MODEL_FILENAME = f"20Chan_{EPOCHS}_{MODEL_SIZE}_best.pth"
+#MODEL_FILENAME = f"./models/100E_1_smoothing/20Chan_{EPOCHS}_{MODEL_SIZE}_E77_07266.pth"
+MODEL_FILENAME = f"./final_runs/100E_20C_BCE_augment_clip_{EPOCHS}.pth"
 
-MODEL_CONFIG_TINY = { # tiny
+MODEL_CONFIG= { # small
     "in_chans": 20,
-    "num_classes": 2,
-    "depths": [3, 3, 9, 3],
-    "dims": [96, 192, 384, 768]
-}
-
-MODEL_CONFIG_SMALL= { # small
-    "in_chans": 20,
-    "num_classes": 2,
+    "num_classes":NUM_CLASSES,
     "depths": [3, 3, 27, 3],
     "dims": [96, 192, 384, 768],
     "drop_path_rate": 0.2  # dropout for residual connections
 }
 
-MODEL_CONFIG_BASE = { # base
-    "in_chans": 20,
-    "num_classes": 2,
-    "depths": [3, 3, 27, 3],
-    "dims": [128, 256, 512, 1024]
-}
 
 RANDOM_STATE = 42
-NORMALISATION_M = 0.1114
-NORMALISATION_SD = 0.2184
+#NORMALISATION_M = 0.1114
+#NORMALISATION_SD = 0.2184
+NORMALISATION_M = 0.5
+NORMALISATION_SD = 0.5
 CHANNELS = 1 # per image (greyscale)
 IMAGE_SIZE = 224
-MODEL_CONFIG = MODEL_CONFIG_TINY
 
-match MODEL_SIZE:
-    case "TINY":
-        MODEL_CONFIG = MODEL_CONFIG_TINY
-    case "SMALL":
-        MODEL_CONFIG = MODEL_CONFIG_SMALL
-    case "BASE":
-        MODEL_CONFIG = MODEL_CONFIG_BASE
-        IMAGE_SIZE = 384
 
 import os 
 
 TRAIN_DIR = os.path.join(DATA_ROOT, "train")
-# TRAIN_AD = os.path.join(TRAIN_DIR, "AD")
-# TRAIN_NC = os.path.join(TRAIN_DIR, "NC")
-
 TEST_DIR = os.path.join(DATA_ROOT, "test")
-# use these for specific evaluations??
-# TEST_AD = os.path.join(TEST_DIR, "AD")
-# TEST_NC = os.path.join(TEST_DIR, "NC")
 
 SAMPLE_IMAGE_FILENAME = "sample_image.png"
 
